d56ce1110ef5ceba1bdcbb2bbb02cd9c0655055d
Lee <mohani7601@naver.com>
2024-05-28 15:52:46 +0900

    modifiy app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import networks
 import streamlit as st
 import tempfile
 import os
-
+from stqdm import stqdm
 
 def get_arguments():
     """Parse all the arguments provided from the CLI.
@@ -29,7 +29,7 @@ def get_arguments():
     return parser.parse_args()
 
 
-def human_parsing(frame, weights, rate, parts):
+def human_parsing(frame, weights, rate, class_of_interest):
     # pascal 데이터 셋 기준
     num_classes = 7
     input_size = [512, 512] 
@@ -88,85 +88,32 @@ def human_parsing(frame, weights, rate, parts):
     frame = transform(frame)
     frame = frame.unsqueeze(0)
     
-    
     with torch.no_grad():
 
         output = model(frame.cuda())   # output은 list 형태
-        
         upsample = torch.nn.Upsample(size=input_size, mode='bilinear', align_corners=True)  ## 세그멘테이션 맵의 크기를 원래 크기로 업샘플링합니다. `torch.nn.Upsample`을 사용하여 bilinear 보간을 통해 업샘플링합니다. 
         upsample_output = upsample(output[0][-1][0].unsqueeze(0))
         upsample_output = upsample_output.squeeze()
         upsample_output = upsample_output.permute(1, 2, 0)  # CHW -> HWC
-
         logits_result = transform_logits(upsample_output.data.cpu().numpy(), c, s, w, h, input_size=input_size)     ## 세그멘테이션 맵을 변환합니다. `transform_logits` 함수를 사용하여 세그멘테이션 맵을 입력 이미지와 동일한 크기로 변환합니다.
         parsing_result = np.argmax(logits_result, axis=2)       ## 로짓 결과에서 클래스에 대한 가장 높은 확률을 가진 인덱스를 가져와 세그멘테이션 결과를 얻습니다.
                                                                 ## parsing_result는 세그멘테이션 맵으로, 각 픽셀에 해당하는 클래스 인덱스를 담고 있는 2차원 배열입니다. 여기서 각 픽셀의 값은 클래스를 나타내는 정수 값입니다.
-        class_of_interest =[]
-        for part in parts:
-            class_of_interest.append(label.index(part))    
-        
-        
-        ## 해당 클래스의 행의 최저, 최대 값과 열의 최저, 최대 값을 구한구 box로 모자이크 처리를 진행 -> 팔 같은 경우 몸통까지 블러처리가 되기때문에 이를 해결해야함
-        
-        row_min = []
-        row_max = []
-        col_min = []
-        col_max = []
-        
-        ymin = len(original_frame)+1        # 몸통 부분을 식별 후 따로 저장하기 위해 사용
-        ymax = -1
-        xmin = len(original_frame[0] +1)
-        xmax = -1
-        
-        for i in range(len(class_of_interest)):
-            row_min.append(len(original_frame)+1)
-            row_max.append(-1)
-            col_min.append(len(original_frame[0])+1)
-            col_max.append(-1)
-        
-        for i in range(0, len(original_frame)):
-             for j in range(0, len(original_frame[0])):     
-                if parsing_result[i][j] in class_of_interest:
-                    idx = class_of_interest.index(parsing_result[i][j])
-                    if row_min[idx] > i:
-                        row_min[idx] = i
-                    if row_max[idx] < i:
-                        row_max[idx] = i
-                    if col_min[idx] > j:
-                        col_min[idx] = j
-                    if col_max[idx] < j:
-                        col_max[idx] = j
-                        
-                
-        
-        for i in range(len(class_of_interest)):
-            if row_min[i] != len(original_frame) + 1 and row_max[i] != -1 and col_min[i] != len(original_frame[0]) + 1 and col_max[i] != -1 and row_min[i] != row_max[i] and col_min[i] != col_max[i]:
-                    
-                frame_part = original_frame[ row_min[i] : row_max[i], col_min[i] : col_max[i]]
 
-                h = row_max[i] - row_min[i]
-                w = col_max[i] - col_min[i]
+        w = len(blur_frame[0])
+        h = len(blur_frame)
+        blur_frame = cv2.resize(blur_frame,(max(w // rate, 1), max(h // rate, 1))) # rate 비율을 사용해 축소
                 
-                frame_part = cv2.resize(frame_part,(max(w // rate, 1), max(h // rate, 1))) # rate(# 모자이크 처리에 사용할 축소 비율 (1 / rate)) 비율을 사용해 축소
+        blur_frame = cv2.resize(blur_frame, (w,h), interpolation=cv2.INTER_AREA)
                 
-                blur_frame_part = cv2.resize(frame_part, (w,h), interpolation=cv2.INTER_AREA)
-                
-                for p in range(row_min[i], row_max[i]):
-                    for q in range(col_min[i], col_max[i]):
-                        if parsing_result[p][q] == 2:       ## 파싱 결과가 몸통이라면
-                            if ymin > i :
-                                ymin = i
-                            if ymax < i:
-                                ymax = i
-                            if xmin > j:
-                                xmin = j
-                            if xmax < j:
-                                xmax = j
-                                
-                blur_frame[ row_min[i] : row_max[i], col_min[i] : col_max[i]] = blur_frame_part
-                blur_frame[ymin : ymax, xmin:xmax] = torso_frame[ymin : ymax, xmin : xmax]         ## 왜 안되냐.. 쓰벌..
-                
+        for i in range(len(blur_frame)):
+            for j in range(len(blur_frame[0])):
+                if parsing_result[i][j] in class_of_interest:
+                    continue
+                else:
+                    blur_frame[i][j] = original_frame[i][j]
+            
     return blur_frame
+
 
 
 def main():
@@ -181,58 +128,76 @@ def main():
     # 업로드된 동영상을 임시 파일로 저장
     temp_file = tempfile.NamedTemporaryFile(delete=False)
     temp_file.write(uploaded_file.read())
-    temp_file.close()    
-    # 동영상 파일 열기
-    cap = cv2.VideoCapture(temp_file.name)
-
-    # 동영상의 FPS와 해상도 가져오기
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-    output_frame = []
-    
-    # Load a model
-    model = YOLO('yolov8n.pt')  # load a pretrained model 
-    model.cuda()
-
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # 동영상의 총 프레임 수 가져오기
-    
-    for _ in tqdm(range(frame_count), desc="Processing frames"):
-        ret, frame = cap.read()
-        if ret:
-            results = model(frame)
-            nms_human = sum(results[0].boxes.cls == 0)  ## 프레임에 사람이 몇명있는가
-            if nms_human > 0:                           ## 프레임 내에 사람이 있다면
-                for bbox in results[0].boxes:
-                    if bbox.cls.item() == 0:            ## 객체 탐지한 것 중 사람만
-                        start_point = (int(bbox.xyxy[0][0].item()), int(bbox.xyxy[0][1].item()))
-                        end_point = (int(bbox.xyxy[0][2].item()), int(bbox.xyxy[0][3].item()))
-
-                        # 휴먼파싱 후 블러처리 까지 하는 함수 호출
-                        blur_frame = human_parsing(frame[ start_point[1] : end_point[1], start_point[0] : end_point[0]], 'exp-schp-201908270938-pascal-person-part.pth', 5, ['Head', 'Lower_Legs' ])
-                        
-                        frame[ start_point[1] : end_point[1], start_point[0] : end_point[0]] = blur_frame
-                # 결과 동영상 파일에 프레임 쓰기    
-            output_frame.append(frame)
-        else:
-            break
-    # 동영상 파일 닫기
-    cap.release()
+    temp_file.close() 
     
     
-    # 결과를 저장할 동영상 파일 설정
+    #'Head', 'Torso', 'Upper_Arms', 'Lower_Arms', 'Upper_Legs', 'Lower_Legs'
+    classes_of_interset = []
+    Head = st.checkbox("머리")
+    Torso = st.checkbox("몸통")
+    Upper_Arms = st.checkbox("팔 윗 부분")
+    Lower_Arms = st.checkbox("팔 아랫 부분")
+    Upper_Legs = st.checkbox("다리 윗 부분")
+    Lower_Legs = st.checkbox("다리 아랫 부분")
+    if Head:
+        classes_of_interset.append(1)
+    if Torso:
+        classes_of_interset.append(2)
+    if Upper_Arms:
+        classes_of_interset.append(3)
+    if Lower_Arms:
+        classes_of_interset.append(4)
+    if Upper_Legs:
+        classes_of_interset.append(5)
+    if Lower_Legs:
+        classes_of_interset.append(6)
+
+    rate = st.slider("모자이크 강도를 설정하세요(0 ~ 50)", 0, 50, 10)
+    
     output_video_path = 'output_video.mp4'
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
-    for frame in output_frame:
-        out.write(frame)
-    out.release()
     
-    # 결과를 저장한 동영상 시각화
-    video_file = open(output_video_path, 'rb')
-    video_bytes = video_file.read()
-    st.video(video_bytes)
+    if st.button("제출"):
+    # 입력을 받은 후 실행할 코드
+        st.write(f"모자이크 강도: {rate}")
+        # 동영상 파일 열기
+        cap = cv2.VideoCapture(temp_file.name)
+
+        # 동영상의 FPS와 해상도 가져오기
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
+        
+        # Load a model
+        model = YOLO('yolov8n.pt')  # load a pretrained model 
+        model.cuda()
+
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # 동영상의 총 프레임 수 가져오기
+        
+        for _ in stqdm(range(frame_count), desc="Processing frames", st_container=st.sidebar):
+            ret, frame = cap.read()
+            if ret:
+                results = model(frame)
+                nms_human = sum(results[0].boxes.cls == 0)  ## 프레임에 사람이 몇명있는가
+                if nms_human > 0:                           ## 프레임 내에 사람이 있다면
+                    for bbox in results[0].boxes:
+                        if bbox.cls.item() == 0:            ## 객체 탐지한 것 중 사람만
+                            start_point = (int(bbox.xyxy[0][0].item()), int(bbox.xyxy[0][1].item()))
+                            end_point = (int(bbox.xyxy[0][2].item()), int(bbox.xyxy[0][3].item()))
+
+                            # 휴먼파싱 후 블러처리 까지 하는 함수 호출
+                            blur_frame = human_parsing(frame[ start_point[1] : end_point[1], start_point[0] : end_point[0]], 'exp-schp-201908270938-pascal-person-part.pth', rate, classes_of_interset)
+                            
+                            frame[ start_point[1] : end_point[1], start_point[0] : end_point[0]] = blur_frame
+                    # 결과 동영상 파일에 프레임 쓰기
+                out.write(frame)
+            else:
+                break
+        # 동영상 파일 닫기
+        cap.release()
+        out.release()
     
     # 다운로드 버튼을 통해 결과 동영상 다운로드 가능
     if st.button('결과 동영상 다운로드'):
